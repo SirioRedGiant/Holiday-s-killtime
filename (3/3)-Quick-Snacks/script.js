2402e64c6a39db9b3cb5e5c4a50983a1fe7ad6da
SirioRedGiant <Oiris1992@gmail.com>
2026-01-06 13:17:31 +0100

    senza l'utilizzo di Math.pow

--- (3/3)-Quick-Snacks/script.js
+++ (3/3)-Quick-Snacks/script.js
@@ -2,5 +2,7 @@
 
 let numberChosed = Number(prompt("Che numero scegli?"));
 for (let i = 1; i <= numberChosed; i++) {
-  console.log(Math.pow(i, 3));
+  //   console.log(Math.pow(i, 3));
+  let previousNumbersCube = i * i * i;
+  console.log("Il cubo di " + i + " è: " + previousNumbersCube);
 }
