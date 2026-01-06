5d0ac6d19a74331a8f6d393086b42e73dc8f4d7f
SirioRedGiant <Oiris1992@gmail.com>
2026-01-06 13:11:04 +0100

    gestione del prompt in modo tale sia un numero

--- (3/3)-Quick-Snacks/script.js
+++ (3/3)-Quick-Snacks/script.js
@@ -1,6 +1,6 @@
 // esercizio 1
 
-let numberChosed = prompt("Che numero scegli?");
+let numberChosed = Number(prompt("Che numero scegli?"));
 for (let i = 1; i <= numberChosed; i++) {
   console.log(Math.pow(i, 3));
 }
