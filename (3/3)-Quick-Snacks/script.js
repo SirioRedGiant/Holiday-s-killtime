f52f9b1cd14fb04c756f53222b913c5c60bdffda
SirioRedGiant <Oiris1992@gmail.com>
2026-01-06 13:47:39 +0100

    exercise 2

--- (3/3)-Quick-Snacks/script.js
+++ (3/3)-Quick-Snacks/script.js
@@ -6,3 +6,12 @@ for (let i = 1; i <= numberChosed; i++) {
   let previousNumbersCube = i * i * i;
   console.log("Il cubo di " + i + " è: " + previousNumbersCube);
 }
+
+// esercizio 2
+
+for (let i = 0; i <= 10; i++) {
+  let checkerRisultato = 2 ** i;
+  if (checkerRisultato <= 1000) {
+    console.log("2 elevato alla " + i + " viene:" + checkerRisultato)
+  }
+}
