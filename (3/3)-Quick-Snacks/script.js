915ab065cb6328505f7e129639803e2cdebcbfbe
SirioRedGiant <Oiris1992@gmail.com>
2026-01-06 13:56:55 +0100

    metodo alternativo: senza conoscre la condizione d'uscita

--- (3/3)-Quick-Snacks/script.js
+++ (3/3)-Quick-Snacks/script.js
@@ -1,5 +1,5 @@
 // esercizio 1
-
+console.log("Primo Esercizio:");
 let numberChosed = Number(prompt("Che numero scegli?"));
 for (let i = 1; i <= numberChosed; i++) {
   //   console.log(Math.pow(i, 3));
@@ -8,10 +8,16 @@ for (let i = 1; i <= numberChosed; i++) {
 }
 
 // esercizio 2
-
+console.log("Secondo Esercizio:");
 for (let i = 0; i <= 10; i++) {
   let checkerRisultato = 2 ** i;
   if (checkerRisultato <= 1000) {
-    console.log("2 elevato alla " + i + " viene:" + checkerRisultato)
+    console.log("2 elevato alla " + i + " viene: " + checkerRisultato);
   }
+}
+//* si poteva fare per uscire i < 10
+//todo: Se avessi voluto evitare di sapere che 2 ** 10 è        superiore a 1000
+console.log("Alternativa:");
+for (let p = 1; p <= 1000; p = p * 2) {
+  console.log(p);
 }
